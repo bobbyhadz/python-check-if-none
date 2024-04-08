9bbe94f4a2cad04003305df0136f77d6dfaba80b
bobbyhadz <opmftw@gmail.com>
2024-04-08 15:25:12 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# ✅ Check if a variable is None
+
+my_var = None
+
+if my_var is None:
+    # 👇️ this runs
+    print('variable is None')
+
+# -----------------------------------------------
+
+# ✅ Check if a variable is NOT None
+
+my_var = 'example'
+
+if my_var is not None:
+    # 👇️ this runs
+    print('variable is not None')
